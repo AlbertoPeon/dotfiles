bb39841b217b13bf10e9d2ec9a7ff7b1d9c34018
Alberto Rodriguez Peon <alberto.rodriguez.peon@gmail.com>
2017-11-29 15:21:37 +0100

    Customize for me

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,10 +3,11 @@
 hub_path=$(which hub)
 if (( $+commands[hub] ))
 then
-  alias git=$hub_path
+  alias ghub=$hub_path
 fi
 
 # The rest of my fun git aliases
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
